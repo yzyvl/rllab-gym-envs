886225885f94f3a5f518d8c81b70e9e39983aed2
智友 杨 <yangzhiyouvl@gmail.com>
2020-11-29 13:43:35 +0800

    Add files via upload

---
+++ mujoco_py/__init__.py
@@ -0,0 +1,7 @@
+from .mjviewer import MjViewer
+from .mjcore import MjModel
+from .mjcore import register_license
+import os
+from .mjconstants import *
+
+register_license('/home/steve/.mujoco/mjkey.txt')
